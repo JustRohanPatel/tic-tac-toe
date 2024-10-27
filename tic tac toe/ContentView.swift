3988d6cab461f00e2b186cdcd69de8f7bee6920f
Rohan Patel <rpatel5@bsd@220.org>
2024-10-27 17:43:07 -0500

    Initial Commit

---
+++ tic tac toe/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  tic tac toe
+//
+//  Created by rohan patel on 10/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Text("Tic Tac Toe")
+                .font(.title).bold()
+            LazyVGrid(columns: Array(repeating: GridItem(.fixed(120)), count: 3), content: {
+                Text("X")
+                Text("X")
+                Text("X")
+                Text("X")
+                Text("X")
+                Text("X")
+                Text("X")
+                Text("X")
+                Text("X")
+                
+            })
+                      }
+        .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tic tac toeTests/tic_tac_toeTests.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeTests.swift
+//  tic tac toeTests
+//
+//  Created by rohan patel on 10/27/24.
+//
+
+import Testing
+@testable import tic_tac_toe
+
+struct tic_tac_toeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
