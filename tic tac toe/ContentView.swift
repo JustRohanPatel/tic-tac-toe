6f61e644ceae3ecba902d5c1ccdf0a71907debf8
Rohan Patel <rpatel5@bsd@220.org>
2024-11-18 09:37:31 -0600

    added x and o

--- tic tac toe/ContentView.swift
+++ tic tac toe/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = Array(repeating: "",count: 9)
+    @State private var xTurn = true
     var body: some View {
         VStack {
             
@@ -17,18 +19,27 @@ struct ContentView: View {
                 ForEach(0..<9) { index in
                     ZStack{
                         Color.blue
-                        Text("X")
+                        Text(moves[index])
                             .font(.system(size: 90))
                             .fontWeight(.heavy)
                     }
                         .frame(width: 120, height: 120,alignment: .center)
                         .cornerRadius(30)
+                        .onTapGesture {
+                            withAnimation(.default) {
+                                if moves[index] == "" {
+                                    moves[index] = xTurn ? "X" : "0"
+                                    xTurn.toggle()
+                                }
+                            }
+                        }
                 }
             })
                       }
+                      
         .preferredColorScheme(.dark)
     }
-}
+                      }
 
 #Preview {
     ContentView()
